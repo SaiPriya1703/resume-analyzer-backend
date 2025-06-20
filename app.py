40d0eb371ca088506c93eca2f8788c26dcc09867
SaiPriya1703 <saipriyapatancheru2gmail.com>
2025-06-20 22:45:44 +0530

    Fix: removed init_db import from app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
-from auth import auth_bp, init_db
+from auth import auth_bp
 from gpt_analyzer import gpt_bp
 import os
 
@@ -16,11 +16,10 @@ jwt = JWTManager(app)
 app.register_blueprint(auth_bp)
 app.register_blueprint(gpt_bp)
 
-# Init DB
-init_db()
 @app.route('/')
 def home():
     return {'message': 'Resume Analyzer Backend is Live 🎯'}
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))  # Use Render's port or default to 5000 locally
     app.run(host='0.0.0.0', port=port)
